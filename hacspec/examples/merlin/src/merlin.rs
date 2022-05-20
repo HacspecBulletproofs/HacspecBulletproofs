d3f173f34a501d98452283fb2549a734484a7033
rasmus-kirk <mail@rasmuskirk.com>
2022-05-20 14:25:54 +0200

    Lots of merlin progress

--- hacspec/examples/merlin/src/merlin.rs
+++ hacspec/examples/merlin/src/merlin.rs
@@ -2,35 +2,41 @@
  * This is a subset of Merlin...
  */
 
-//#![allow(dead_code)]
-//#![allow(unused_imports)]
-//#![allow(non_snake_case)]
-//#![allow(unused_assignments)]
-//#![allow(unused_variables)]
-
-mod strobe;
+pub mod strobe;
 use hacspec_lib::*;
-use crate::strobe::*;
+use strobe::*;
 
 pub type Transcript = Strobe;
 
-pub fn new(init: Seq<U8>) -> Transcript {
-	new_strobe(init)
+fn MERLIN_PROTOCOL_LABEL() -> Seq<U8> {
+	byte_seq!(77u8, 101u8, 114u8, 108u8, 105u8, 110u8, 32u8, 118u8, 49u8, 46u8, 48u8)
 }
 
-pub fn encode_U64(x: U64) -> Seq<U8> {
+// === Internal Functions === //
+
+fn encode_U64(x: U64) -> Seq<U8> {
 	U64_to_le_bytes(x).to_le_bytes()
 }
 
-pub fn encode_usize_as_u32(x: usize) -> Seq<U8> {
+fn encode_usize_as_u32(x: usize) -> Seq<U8> {
 	let x_U32 = U32::classify(x as u32);
 	U32_to_le_bytes(x_U32).to_le_bytes()
 }
 
+// === Public Functions === //
+
+pub fn new(label: Seq<U8>) -> Transcript {
+	let transcript = new_strobe(MERLIN_PROTOCOL_LABEL());
+	// b"dom-sep"
+	let dom_sep = byte_seq!(100u8, 111u8, 109u8, 45u8, 115u8, 101u8, 112u8);
+
+	append_message(transcript, dom_sep, label)
+}
+
 // Strobe op: meta-AD(label || len(message)); AD(message)
 pub fn append_message(mut transcript: Transcript, label: Seq<U8>, message: Seq<U8>) -> Transcript {
-	let l = message.len();
 	let data_len = U32_to_le_bytes(U32::classify(message.len() as u32)).to_be_bytes();
+
 	transcript = meta_ad(transcript, label, false);
 	transcript = meta_ad(transcript, data_len, true);
 	transcript = ad(transcript, message, false);
@@ -38,18 +44,14 @@ pub fn append_message(mut transcript: Transcript, label: Seq<U8>, message: Seq<U
 }
 
 pub fn challenge_bytes(mut transcript: Transcript, label: Seq<U8>, dest: Seq<U8>) -> (Transcript, Seq<U8>) {
-	let prf_len = dest.len();
+	let data_len = encode_usize_as_u32(dest.len());
 
-	// metadata = label || len(challenge_bytes);
-	let mut metadata: Seq<U8> = Seq::<U8>::new(label.len() + 4);
-	metadata.concat(&label);
-	metadata.concat(&encode_usize_as_u32(prf_len));
-
-	transcript = meta_ad(transcript, metadata, false);
+	transcript = meta_ad(transcript, label, false);
+	transcript = meta_ad(transcript, data_len, true);
 	prf(transcript, dest, false)
 }
 
 // Strobe op: meta-AD(label || len(message)); AD(message)
-pub fn append_u64(mut transcript: Transcript, label: Seq<U8>, x: U64) -> Transcript {
+pub fn append_u64(transcript: Transcript, label: Seq<U8>, x: U64) -> Transcript {
 	append_message(transcript, label, encode_U64(x))
 }
