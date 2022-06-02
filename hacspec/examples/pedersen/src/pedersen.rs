75585d5e2d61ade540527ef408a2217294571e05
Anders Wibrand Larsen <au633424 @uni.au.dk>
2022-06-02 11:58:03 +0200

    Fixed Pedersen commitments

--- hacspec/examples/pedersen/src/pedersen.rs
+++ hacspec/examples/pedersen/src/pedersen.rs
@@ -1,29 +1,28 @@
+#![allow(non_snake_case)]
 use hacspec_lib::*;
 use hacspec_ristretto::*;
 use hacspec_linalg_field::*;
-use hacspec_ristretto as ristretto;
-use hacspec_linalg_field as linalg;
 
-//We need to generate better Base Points
-fn point_dot(v: Matrix, p: RistrettoPoint, G: RistrettoPoint) -> RistrettoPoint {
+fn point_dot(v: Matrix, G: RistrettoPoint) -> RistrettoPoint {
 	let mut acc = IDENTITY_POINT();
-	for i in 0..v.1.len() {
-		acc = ristretto::add(acc, ristretto::mul(v.1[i], G));
+	let (_, entries) = v; 
+	for i in 0.. entries.len() {
+		acc = add(acc, mul(entries[i], G));
 	}
 	acc
 }
 
 //r must be random
 pub fn pedersen_commit(r: Scalar, G: RistrettoPoint, H: RistrettoPoint, a: Scalar) -> RistrettoPoint {
-	let rH = ristretto::mul(r,H);
-	let aG = ristretto::mul(a, G);
+	let rH = mul(r,H);
+	let aG = mul(a, G);
 
-	ristretto::add(aG, rH)
+	add(aG, rH)
 }
 
 pub fn vector_pedersen_commit(r: Scalar, G: RistrettoPoint, H: RistrettoPoint, v: Matrix) -> RistrettoPoint {
-	let rH = ristretto::mul(r,H);
-	let vG = point_dot(v, G, G);
+	let rH = mul(r,H);
+	let vG = point_dot(v, G);
 
-	ristretto::add(rH, vG)
+	add(rH, vG)
 }
