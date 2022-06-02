45de0c5628f58a1c3b6fbdd4edb71de9f9b2f5c6
Anders Wibrand Larsen <au633424 @uni.au.dk>
2022-06-02 11:58:37 +0200

    Renamed some functions for unambiguity in Pedersen commits

--- hacspec/examples/linalg-field/src/linalg.rs
+++ hacspec/examples/linalg-field/src/linalg.rs
@@ -1,11 +1,11 @@
 use hacspec_lib::*;
 
 pub type DimType = usize;
-pub type Scalar = hacspec_ristretto::Scalar;
+pub type MatrixEntry = hacspec_ristretto::Scalar;
 pub type Dims = (DimType, DimType);
-pub type Matrix = (Dims, Seq<Scalar>);
+pub type Matrix = (Dims, Seq<MatrixEntry>);
 
-pub fn new(rows: DimType, cols: DimType, seq: Seq<Scalar>) -> Result<Matrix, ()> {
+pub fn matrix_new(rows: DimType, cols: DimType, seq: Seq<MatrixEntry>) -> Result<Matrix, ()> {
     if seq.len() > 0 && rows * cols == seq.len() {
         Result::<Matrix, ()>::Ok(((rows, cols), seq))
     } else {
@@ -14,46 +14,46 @@ pub fn new(rows: DimType, cols: DimType, seq: Seq<Scalar>) -> Result<Matrix, ()>
 }
 
 pub fn zeros(n: DimType, m: DimType) -> Result<Matrix, ()> {
-    new(n, m, Seq::<Scalar>::new(n * m))
+    matrix_new(n, m, Seq::<MatrixEntry>::new(n * m))
 }
 
 pub fn ones(n: DimType, m: DimType) -> Result<Matrix, ()> {
-    let mut ret = Seq::<Scalar>::new(n * m);
+    let mut ret = Seq::<MatrixEntry>::new(n * m);
 
     for i in 0..n * m {
-        ret[i] = Scalar::ONE();
+        ret[i] = MatrixEntry::ONE();
     }
 
-    new(n, m, ret)
+    matrix_new(n, m, ret)
 }
 
 pub fn identity(n: DimType) -> Result<Matrix, ()> {
-    let mut ret = Seq::<Scalar>::new(n * n);
+    let mut ret = Seq::<MatrixEntry>::new(n * n);
 
     for i in 0..n {
         let index = i * n + i;
-        ret[index] = Scalar::ONE();
+        ret[index] = MatrixEntry::ONE();
     }
 
-    new(n, n, ret)
+    matrix_new(n, n, ret)
 }
 
-pub fn index(m: Matrix, i: DimType, j: DimType) -> Result<Scalar, ()> {
+pub fn index(m: Matrix, i: DimType, j: DimType) -> Result<MatrixEntry, ()> {
     let (dim, seq) = m;
     let (rows, cols) = dim;
     let index = i * cols + j;
 
     if index >= rows * cols {
-        Result::<Scalar, ()>::Err(())
+        Result::<MatrixEntry, ()>::Err(())
     } else {
-        Result::<Scalar, ()>::Ok(seq[index])
+        Result::<MatrixEntry, ()>::Ok(seq[index])
     }
 }
 
 pub fn transpose(matrix: Matrix) -> Matrix {
     let (dim, seq) = matrix;
     let (rows, cols) = dim;
-    let mut ret = Seq::<Scalar>::new(seq.len());
+    let mut ret = Seq::<MatrixEntry>::new(seq.len());
 
     for i in 0..rows {
         for j in 0..cols {
@@ -72,7 +72,7 @@ pub fn slice(matrix: Matrix, start: Dims, len: Dims) -> Result<Matrix, ()> {
     let (start_row, start_col) = start;
     let (len_rows, len_cols) = len;
     let start_index = start_row * cols + start_col;
-    let mut ret = Seq::<Scalar>::new(len_rows * len_cols);
+    let mut ret = Seq::<MatrixEntry>::new(len_rows * len_cols);
     let mut res = Result::<Matrix, ()>::Err(());
 
     if start_index + len_rows * len_cols <= rows * cols {
@@ -84,15 +84,15 @@ pub fn slice(matrix: Matrix, start: Dims, len: Dims) -> Result<Matrix, ()> {
             }
         }
 
-        res = new(len_rows, len_cols, ret);
+        res = matrix_new(len_rows, len_cols, ret);
     }
 
     res
 }
 
-pub fn scale(matrix: Matrix, scalar: Scalar) -> Matrix {
+pub fn matrix_scale(matrix: Matrix, scalar: MatrixEntry) -> Matrix {
     let (dim, seq) = matrix;
-    let mut ret = Seq::<Scalar>::new(seq.len());
+    let mut ret = Seq::<MatrixEntry>::new(seq.len());
 
     for i in 0..seq.len() {
         ret[i] = scalar * seq[i].clone()
@@ -101,10 +101,10 @@ pub fn scale(matrix: Matrix, scalar: Scalar) -> Matrix {
     (dim, ret)
 }
 
-pub fn add(matrix_1: Matrix, matrix_2: Matrix) -> Result<Matrix, ()> {
+pub fn matrix_add(matrix_1: Matrix, matrix_2: Matrix) -> Result<Matrix, ()> {
     let (m1_dim, m1_s) = matrix_1;
     let (m2_dim, m2_s) = matrix_2;
-    let mut ret = Seq::<Scalar>::new(m1_s.len());
+    let mut ret = Seq::<MatrixEntry>::new(m1_s.len());
     let mut res = Result::<Matrix, ()>::Err(());
 
     if m1_dim == m2_dim {
@@ -116,10 +116,10 @@ pub fn add(matrix_1: Matrix, matrix_2: Matrix) -> Result<Matrix, ()> {
     res
 }
 
-pub fn sub(matrix_1: Matrix, matrix_2: Matrix) -> Result<Matrix, ()> {
+pub fn matrix_sub(matrix_1: Matrix, matrix_2: Matrix) -> Result<Matrix, ()> {
     let (m1_dim, m1_s) = matrix_1;
     let (m2_dim, m2_s) = matrix_2;
-    let mut ret = Seq::<Scalar>::new(m1_s.len());
+    let mut ret = Seq::<MatrixEntry>::new(m1_s.len());
     let mut res = Result::<Matrix, ()>::Err(());
 
     if m1_dim == m2_dim {
@@ -131,10 +131,10 @@ pub fn sub(matrix_1: Matrix, matrix_2: Matrix) -> Result<Matrix, ()> {
     res
 }
 
-pub fn component_mul(matrix_1: Matrix, matrix_2: Matrix) -> Result<Matrix, ()> {
+pub fn matrix_component_mul(matrix_1: Matrix, matrix_2: Matrix) -> Result<Matrix, ()> {
     let (m1_dim, m1_s) = matrix_1;
     let (m2_dim, m2_s) = matrix_2;
-    let mut ret = Seq::<Scalar>::new(m1_s.len());
+    let mut ret = Seq::<MatrixEntry>::new(m1_s.len());
     let mut res = Result::<Matrix, ()>::Err(());
 
     if m1_dim == m2_dim {
@@ -146,18 +146,18 @@ pub fn component_mul(matrix_1: Matrix, matrix_2: Matrix) -> Result<Matrix, ()> {
     res
 }
 
-pub fn mul(matrix_1: Matrix, matrix_2: Matrix) -> Result<Matrix, ()> {
+pub fn matrix_mul(matrix_1: Matrix, matrix_2: Matrix) -> Result<Matrix, ()> {
     let (dim_1, seq_1) = matrix_1;
     let (dim_2, seq_2) = matrix_2;
     let (m, n) = dim_1;
     let (n_2, p) = dim_2;
-    let mut ret = Seq::<Scalar>::new(m * p);
+    let mut ret = Seq::<MatrixEntry>::new(m * p);
     let mut res = Result::<Matrix, ()>::Err(());
 
     if n == n_2 {
         for i in 0..m {
             for j in 0..p {
-                let mut acc = Scalar::ZERO();
+                let mut acc = MatrixEntry::ZERO();
                 let index = i * p + j;
 
                 for k in 0..n {
@@ -171,7 +171,7 @@ pub fn mul(matrix_1: Matrix, matrix_2: Matrix) -> Result<Matrix, ()> {
             }
         }
 
-        res = new(m, p, ret)
+        res = matrix_new(m, p, ret)
     }
 
     res
